83817d6aff3c0bd7f3405be403dc8338b0a8e2d6
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-27 03:01:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,15 +2,13 @@ list = ["ab", "cd", "ef"]
 
 def my_each(array)
   i = 0
-  while i < array.length 
-  
+  while i < array.length
     yield(array[i])
     i = i + 1
   end
 end
 
 my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
-
 
 def hello(array)
   i = 0
